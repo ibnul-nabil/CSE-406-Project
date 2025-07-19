17f67a75d8590fbcac883522cd6a3754be37c522
tausif <tausif.rashid21@gmail.com>
2025-07-19 15:12:08 +0600

    bug fix

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import tcp_scratch as ts
 import socket
 
 if __name__ == "__main__":
-
-    src_ip = "192.168.10.151"
+    
+    src_ip = "192.168.10.55"
     src_port = 20
-    dst_ip = "192.168.10.55"
+    dst_ip = "192.168.10.151"
     dst_port = 666
 
     packet = ts.TCPPacket(src_ip, src_port, dst_ip, dst_port)
