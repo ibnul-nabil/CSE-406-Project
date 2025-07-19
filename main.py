ed794796bef8675ddf28dbd1d6b5dfed3da88f33
ibnul-nabil <2005060@ugrad.cse.buet.ac.bd>
2025-07-19 16:57:47 +0600

    "ip packet error fixed + msg portion separated from tcp"

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import tcp_scratch as ts
 import ip_scratch as ips
+import struct
 import socket
 
 if __name__ == "__main__":
 
-    src_ip = "192.168.10.153"
+    src_ip = "192.168.10.151"
     src_port = 20
     dst_ip = "192.168.10.55"
     dst_port = 666
 
-    segment = ts.TCPPacket(src_ip, src_port, dst_ip, dst_port , "helo").build()
-    packet = ips.IPPacket(src_ip, dst_ip).build() + segment
+    msg = input("Enter your msg: ")
+    msg_bytes = msg.encode()[:64]
+    msg_bytes = msg_bytes.ljust(64, b'\x00')  # Pad to 32 bytes
+    data = struct.pack('!64B', *msg_bytes)
 
+    segment = ts.TCPPacket(src_ip, src_port, dst_ip, dst_port).build()
+    packet = ips.IPPacket(src_ip, dst_ip).build() + segment + data
 
-
-    # s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-
-    # s.sendto(packet.build(), (dst_ip, 0)) 
 
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
     s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
